45ce6d28792734d08ce362926cf10076ccac1d31
Pedro <p@frame.xyz>
2024-08-30 21:59:59 -0600

    Rebrand Commit.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,27 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
+      backgroundImage: (_theme) => ({
+        "gradient-primary":
+          "linear-gradient(106.09deg, #F6F6F5 -3.46%, #E7EAEF 11.66%, #D1D5DE 22.41%, #E3E7EE 32.93%, #FFFFFF 63.13%, #D5DAE1 77.68%, #F7F9FD 94.11%)",
+      }),
       fontFamily: {
-        sans: ['Adelle Sans', ...defaultTheme.fontFamily.sans],
+        sans: ["Roobert", ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        'privy-navy': '#160B45',
-        'privy-light-blue': '#EFF1FD',
-        'privy-blueish': '#D4D9FC',
-        'privy-pink': '#FF8271',
+        "privy-navy": "#160B45",
+        "privy-light-blue": "#EFF1FD",
+        "privy-blueish": "#D4D9FC",
+        "privy-pink": "#FF8271",
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require("@tailwindcss/forms")],
 };
